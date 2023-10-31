5231a461e87bf0169973fe7b8cd0b46b670a82c0
Saurabh Saxena <saurabh.jan03@gmail.com>
2023-10-31 01:59:46 -0400

    HashMap

---
+++ src/main/java/org/CoreJava/HashMap.java
@@ -0,0 +1,23 @@
+package org.CoreJava;
+
+public class HashMap {
+
+    public static void main(String[] args) {
+        java.util.HashMap<Character, Integer> map = new java.util.HashMap<>();
+        String s = "SaurabhSaxena";
+
+       /* List<String> list = new ArrayList<>();
+        list.stream().sorted((o1, o2) -> o1.compareTo(o2)).collect(Collectors.toList());
+        System.out.println(list);*/
+
+        for(int i =0; i< s.length();i++){
+            if (map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i), map.get(s.charAt(i))+1);
+            }
+            else {
+                map.put(s.charAt(i), 1);
+            }
+        }
+        System.out.println(map);
+    }
+}
