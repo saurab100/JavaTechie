d1426e6153802be0ff24d8f9d77324794267230d
Saurabh Saxena <saurabh.jan03@gmail.com>
2023-11-18 22:35:18 -0500

    Reverse a string in place

---
+++ src/main/java/org/CoreJava/Strings/ReverseString.java
@@ -0,0 +1,25 @@
+package org.CoreJava.Strings;
+
+//How do you reverse a given string in place?
+public class ReverseString {
+
+    public static void main(String[] args) {
+        String s = "Saurabh";
+        char[] arr = s.toCharArray();
+        int i = 0;
+        int j = arr.length - 1;
+        while (i < j) {
+            swap(arr, i, j);
+            i++;
+            j--;
+        }
+        System.out.println(arr);
+    }
+
+
+    private static void swap(char[] str, int i, int j) {
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+}
