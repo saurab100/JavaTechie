8c7b3c90cb804d4770732a5ed0c8b5ee9abd139e
Saurabh Saxena <saurabh.jan03@gmail.com>
2023-11-19 01:27:05 -0500

    RemoveString

--- src/main/java/org/leetcode/Running_Sum.java
+++ src/main/java/org/leetcode/Running_Sum.java
@@ -1,5 +1,11 @@
 package org.leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Running_Sum {
         public int[] runningSum(int[] nums) {
             int[] arr = new int[nums.length];
@@ -9,4 +15,13 @@ public class Running_Sum {
             }
             return arr;
         }
+
+    public static void main(String[] args) {
+        String s = "my name is saurabh saxena and saurabh is a very my name and he is very good";
+        String[] words = s.split(" ");
+        List<String> list = Arrays.asList(words);
+        list.stream().filter(s1 -> s1.length() > 3)
+                .distinct()
+                .forEach(System.out::println);
+    }
     }
