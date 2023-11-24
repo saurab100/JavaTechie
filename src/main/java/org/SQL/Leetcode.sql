130334b3e13c646ab4e2d2c6bf126d0745d4dc9e
Saurabh Saxena <saurabh.jan03@gmail.com>
2023-11-24 02:21:02 -0500

    SQL

--- src/main/java/org/SQL/Leetcode.sql
+++ src/main/java/org/SQL/Leetcode.sql
@@ -12,3 +12,11 @@ from visits v left join Transactions t on
 v.visit_id = t.visit_id
 where t.transaction_id is NULL
 group by v.customer_id
+
+3. Number of Unique Subjects Taught by Each Teacher
+select teacher_id, count(distinct subject_id) as cnt
+from teacher  group by teacher_id
+
+4. Classes More Than 5 Students
+select class from courses
+group by class having count(student) >= 5
